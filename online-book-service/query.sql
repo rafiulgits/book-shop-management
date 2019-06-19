144d1269e22d8e7ac9c3814cb8da4cebfb2bf961
Rafiul Islam <avoidcloud@gmail.com>
2019-06-19 12:34:13 +0600

    query functions defining

--- online-book-service/query.sql
+++ online-book-service/query.sql
@@ -1,35 +1,35 @@
-<?php
 
-// search with category name
+
+-- // search with category name
 select isbn, name, edition, price from book where category_id = 
 (select id from category where name ilike 'arts' limit 1);
 
-// join author name with book information
+-- // join author name with book information
 select book.isbn, book.name, book.edition, book.price, author.name
 from book inner join author on book.author_id=author.id;
 
-// book info with author name filter by category name
+-- // book info with author name filter by category name
 select book.isbn, book.name, book.edition, book.price, author.name
 from book inner join author on book.author_id=author.id
 where book.category_id=(select id from category where name ilike 'arts' limit 1);
 
-// book stock information
+-- // book stock information
 select book.isbn, book.name, book.edition, book.edition, stock.entry_copy, stock.sold_copy
 from book inner join stock on book.stock_id = stock.id;
 
-// book stock information customize with remaining copy
+-- // book stock information customize with remaining copy
 select book.isbn, book.name, book.edition,stock.entry_copy-stock.sold_copy as remain
 from book inner join stock on book.stock_id = stock.id;
 
 
-// book informations from multiple table using multiple join
+-- // book informations from multiple table using multiple join
 select book.name,book.price,author.name,publisher.name
 from book 
 inner join author on book.author_id=author.id
 inner join publisher on book.publisher_id=publisher.id;
 
 
-// book information from multiple table filter by multiple keyword
+-- // book information from multiple table filter by multiple keyword
 select book.name,book.price,author.name,publisher.name
 from book 
 inner join author on book.author_id=author.id
@@ -40,7 +40,7 @@ where
 	book.author_id=(select id from author where name ilike '%stark%');
 
 
-// search book by price range
+-- // search book by price range
 select book.name,book.price,author.name,publisher.name
 from book 
 inner join author on book.author_id=author.id
@@ -48,29 +48,24 @@ inner join publisher on book.publisher_id=publisher.id
 where book.price>100 and book.price<=150;
 
 
-// update book price with current price percentage (10% example)
+-- // update book price with current price percentage (10% example)
 update book
 set price=(price-(price*0.10))
 where price > 120;
 
-// counting the available book of every author
+-- // counting the available book of every author
 select (author.name) "Author", count(book.author_id) "Available Books"
 from author left join book on author.id=book.author_id
 group by author.name;
 
 
-// counting the available book of every category
+-- // counting the available book of every category
 select (category.name) "Category", count(book.category_id) "Available Book"
 from category left join book on category.id=book.category_id
 group by category.name;
 
 
-// filter the unavailable books of an category
+-- // filter the unavailable books of an category
 select (name) "Empty Book" from category
 where id in
 (select id from category except select category_id from book);
-
-
-
-
-?>
